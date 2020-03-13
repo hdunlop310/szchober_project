e283461f34074a3414cf7f8595759ce96d95e53f
heather-cook10 <59927681+heather-cook10@users.noreply.github.com>
2020-03-13 11:02:06 +0000

    changed models

--- szchober/models.py
+++ szchober/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
     phone_number = models.CharField(max_length=11)
     rating = models.IntegerField(default=5)
     review = models.CharField(max_length=128)
+    user_id = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return self.name
